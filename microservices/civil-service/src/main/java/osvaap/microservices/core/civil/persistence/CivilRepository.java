e6074f9b4ee5be26d03cab631b5dccd563bc8087
Oswaldo <bigosvaap@gmail.com>
2022-03-11 10:47:35 -0600

    Small change

--- microservices/civil-service/src/main/java/osvaap/microservices/core/civil/persistence/CivilRepository.java
+++ microservices/civil-service/src/main/java/osvaap/microservices/core/civil/persistence/CivilRepository.java
@@ -1,9 +1,9 @@
 package osvaap.microservices.core.civil.persistence;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CivilRepository extends PagingAndSortingRepository<CivilEntity, String> {
+public interface CivilRepository extends CrudRepository<CivilEntity, String> {
     Optional<CivilEntity> findByRFC(String RFC);
 }
